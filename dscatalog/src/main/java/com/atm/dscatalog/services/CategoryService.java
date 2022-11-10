77052e07deec4c65080563d5ff0942d128310965
atircio <atircio12@gmail.com>
2022-11-10 16:35:22 +0100

    Data Seeding

--- dscatalog/src/main/java/com/atm/dscatalog/services/CategoryService.java
+++ dscatalog/src/main/java/com/atm/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.atm.dscatalog.entities.Category;
 import com.atm.dscatalog.repositories.CategoryRepository;
@@ -11,9 +12,10 @@ import com.atm.dscatalog.repositories.CategoryRepository;
 @Service
 public class CategoryService {
 	
-	@Autowired
+	@Autowired/*Essa annotation garante q a variavel seja uma dependencia de CategoryRepository*/
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)/*Garante que este metodo ganhe um dos atributos do ACID(Faz tudo ou não faz nada) ou garante q faça uma transação*/
 	public List<Category> findAll(){
 		return repository.findAll();
 	}
