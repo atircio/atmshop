8353aa4f47af4d905c225cd04e6b55a527d8e4f1
atircio <atircio12@gmail.com>
2022-11-10 16:58:41 +0100

    CategoryDTO

--- dscatalog/src/main/java/com/atm/dscatalog/services/CategoryService.java
+++ dscatalog/src/main/java/com/atm/dscatalog/services/CategoryService.java
@@ -1,22 +1,33 @@
 package com.atm.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.atm.dscatalog.dto.CategoryDTO;
 import com.atm.dscatalog.entities.Category;
 import com.atm.dscatalog.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
-	
-	@Autowired/*Essa annotation garante q a variavel seja uma dependencia de CategoryRepository*/
+
+	@Autowired /*
+				 * Essa annotation garante q a variavel seja uma dependencia de
+				 * CategoryRepository
+				 */
 	private CategoryRepository repository;
-	
-	@Transactional(readOnly = true)/*Garante que este metodo ganhe um dos atributos do ACID(Faz tudo ou não faz nada) ou garante q faça uma transação*/
-	public List<Category> findAll(){
-		return repository.findAll();
+
+	@Transactional(readOnly = true) /*
+									 * Garante que este metodo ganhe um dos atributos do ACID(Faz tudo ou não faz
+									 * nada) ou garante q faça uma transação
+									 */
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+
+		List<CategoryDTO> listDto = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		return listDto;
 	}
 }
