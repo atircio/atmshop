33bbda5692e32fd27bbffcd9a840d6fd8576c245
atircio <atircio12@gmail.com>
2022-11-17 23:32:31 +0100

    PUT (update) Category

--- dscatalog/src/main/java/com/atm/dscatalog/resources/CategoryResource.java
+++ dscatalog/src/main/java/com/atm/dscatalog/resources/CategoryResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,33 +19,41 @@ import com.atm.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
-public class CategoryResource  {
-	
+public class CategoryResource {
+
 	@Autowired
 	private CategoryService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<CategoryDTO>> findAll(){
+	public ResponseEntity<List<CategoryDTO>> findAll() {
 		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
-		
+
 	}
-	
-	@GetMapping(value ="/{id}")
-	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){/*pathvariable serve para casar a variavel id com a id fo getmappin*/
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {/* pathvariable serve para casar a variavel id com a id fo getmappin */
 		CategoryDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
-		
+
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
 		dto = service.insert(dto);
-		
+
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
-		
+
 		return ResponseEntity.created(uri).body(dto);
-		
+
 	}
 	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<CategoryDTO> update( @PathVariable Long id, @RequestBody CategoryDTO dto) {
+		dto = service.update(dto,id);
+
+		return ResponseEntity.ok().body(dto);
+
+	}
+
 }
--- dscatalog/src/main/java/com/atm/dscatalog/services/CategoryService.java
+++ dscatalog/src/main/java/com/atm/dscatalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -12,7 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.atm.dscatalog.dto.CategoryDTO;
 import com.atm.dscatalog.entities.Category;
 import com.atm.dscatalog.repositories.CategoryRepository;
-import com.atm.dscatalog.services.exceptions.EntityNotFoundException;
+import com.atm.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -37,7 +39,7 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	}
 
@@ -48,6 +50,20 @@ public class CategoryService {
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(CategoryDTO dto, Long id) {
+		Optional<Category> obj = repository.findById(id);
+		try {
+			Category entity = obj.orElseThrow();
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found: "+e);
+		}
+
 	}
 
 }
